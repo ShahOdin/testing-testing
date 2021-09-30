f356d043ec3498e67a05e1396d4de34cf10e810c
shahsara <13497500+ShahOdin@users.noreply.github.com>
2021-09-30 17:45:26 +0100

    adding lawfulness tests.

--- src/test/scala/logic/GameEngineV1LawfulnessSpec.scala
+++ src/test/scala/logic/GameEngineV1LawfulnessSpec.scala
@@ -1,11 +1,14 @@
-package laws
+package logic
 
-import cats.effect.IO
 import arbitrary._
+import cats.effect.IO
+import laws.GameEngineV1AlegraTests
+import laws._
+import util.Scenarios
 
 class GameEngineV1LawfulnessSpec extends munit.CatsEffectSuite with munit.DisciplineSuite {
   checkAll(
-    "GameEngine.V1",
+    "GameEngine.V1".tag(Scenarios.Laws),
     GameEngineV1AlegraTests[IO].algebra
   )
 }
