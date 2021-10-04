bf55c0510d1a87a3b3f812b1f03428686c573f24
shahsara <13497500+ShahOdin@users.noreply.github.com>
2021-10-04 17:47:27 +0100

    added additional laws

--- src/test/scala/laws/GameEngineV1AlegraTests.scala
+++ src/test/scala/laws/GameEngineV1AlegraTests.scala
@@ -4,7 +4,7 @@ import cats.Eq
 import cats.effect.MonadCancel
 import cats.effect.kernel.Resource
 import logic.GameEngine
-import models.PlayerInput
+import models.{PlayerData, PlayerInput}
 import org.scalacheck.{Arbitrary, Prop}
 import org.typelevel.discipline.Laws
 import cats.kernel.laws.discipline._
@@ -16,12 +16,25 @@ trait GameEngineV1AlegraTests[F[_]] extends Laws {
       implicit
       arbInput: Arbitrary[PlayerInput],
       eqFSetInput: Eq[F[Set[PlayerInput]]],
+      eqFPlayerData: Eq[F[PlayerData]],
       gameEngine: Arbitrary[Resource[F, GameEngine.V1[F]]]
   ): SimpleRuleSet = new SimpleRuleSet(
     name = "GameEngine.V1",
     "process() keeps a copy of the processed input." -> Prop.forAll {
       (input: PlayerInput, gameEngineResource: Resource[F, GameEngine.V1[F]]) =>
         laws.processSavesUserInput(gameEngineResource)(input)
+    },
+    "process ignores MoveUp at maxY." -> Prop.forAll { gameEngineResource: Resource[F, GameEngine.V1[F]] =>
+      laws.moveUpIsIgnoredAtMaxY(gameEngineResource)
+    },
+    "process ignores MoveDown at minY." -> Prop.forAll { gameEngineResource: Resource[F, GameEngine.V1[F]] =>
+      laws.moveDownIsIgnoredAtMinY(gameEngineResource)
+    },
+    "process ignores MoveRight at maxX." -> Prop.forAll { gameEngineResource: Resource[F, GameEngine.V1[F]] =>
+      laws.moveRightIsIgnoredAtMaxX(gameEngineResource)
+    },
+    "process ignores MoveLeft at minX." -> Prop.forAll { gameEngineResource: Resource[F, GameEngine.V1[F]] =>
+      laws.moveLeftIsIgnoredAtMinX(gameEngineResource)
     }
   )
 }
